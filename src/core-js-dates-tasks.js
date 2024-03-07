293e5dd855783dbedaeb5fa9f041cf6fb73df52f
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-03-07 21:31:50 +0300

    feat: solution of 2 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -145,8 +145,8 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en-US', { timeZone: 'UTC' });
 }
 
 /**
@@ -161,8 +161,16 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let count = 0;
+  const countOfDays = new Date(year, month, 0).getDate();
+  for (let i = 1; i <= countOfDays; i += 1) {
+    const day = new Date(year, month - 1, i).getDay();
+    if (day === 0 || day === 6) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
